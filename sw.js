6645760c279ae0dc32a8415778a95074aa630ba2
rafeashboul2-creator <rafea.shboul2@gmail.com>
2026-07-28 14:42:18 +0300

    Add service worker for asset caching
    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'health-app-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
